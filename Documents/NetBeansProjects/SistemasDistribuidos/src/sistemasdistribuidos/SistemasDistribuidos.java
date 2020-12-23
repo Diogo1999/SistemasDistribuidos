687f747882f3e6f6985b0a4096d226b2d851c851
User <User@LAPTOP-7PQBK0MQ>
2020-12-23 15:02:14 +0000

    Acdicionei nada de jeito

--- Documents/NetBeansProjects/SistemasDistribuidos/src/sistemasdistribuidos/SistemasDistribuidos.java
+++ Documents/NetBeansProjects/SistemasDistribuidos/src/sistemasdistribuidos/SistemasDistribuidos.java
@@ -20,6 +20,7 @@ public class SistemasDistribuidos {
         System.out.println("teste");
         System.out.println("Diogo");
         System.out.println("TESTE");
+        System.out.println("DIOGOCHEIRAMAL");
     }
     
 }
